97b06b7d32f1ef7c0d0f588cc844941dc9966874
Rohan Kevin Broach <39668340+rkbroach@users.noreply.github.com>
2019-08-19 19:20:05 +0530

    Add files via upload

---
+++ mathematics/add-two-fractions.cpp
@@ -0,0 +1,40 @@
+/*
+
+██████╗ ██╗  ██╗██████╗ 
+██╔══██╗██║ ██╔╝██╔══██╗
+██████╔╝█████╔╝ ██████╔╝
+██╔══██╗██╔═██╗ ██╔══██╗
+██║  ██║██║  ██╗██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
+
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+ll gcd(ll a, ll b) {
+    // modulo operator in Euclidean algorithm
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a%b);
+}
+
+ll lcm(ll a, ll b) {
+    return (  (a*b) / gcd(a,b)  );
+}
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int T; cin >> T;
+	while (T--) {
+		ll a, b, x, y; cin >> a >> b;
+		x = lcm(a,b);
+		y = gcd(a,b);
+		
+		cout << x << " " << y << "\n";
+		
+    }
+	return 0;
+}
