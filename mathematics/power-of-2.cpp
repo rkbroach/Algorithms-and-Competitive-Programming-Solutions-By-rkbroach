5c1253a33f3b34eef5d5a9bc4d409342b8427336
Rohan Kevin Broach <39668340+rkbroach@users.noreply.github.com>
2019-09-06 14:53:26 +0530

    Update power-of-2.cpp

--- mathematics/power-of-2.cpp
+++ mathematics/power-of-2.cpp
@@ -7,34 +7,27 @@
 ██║  ██║██║  ██╗██████╔╝
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
 
+Problem Statement - https://practice.geeksforgeeks.org/problems/power-of-2/0/?track=sp-mathematics&batchId=152
+
 */
 
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
 
-ll gcd(ll a, ll b) {
-    // modulo operator in Euclidean algorithm
-    if (b == 0) {
-        return a;
-    }
-    return gcd(b, a%b);
-}
-
-ll lcm(ll a, ll b) {
-    return (  (a*b) / gcd(a,b)  );
-}
-
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
 	int T; cin >> T;
 	while (T--) {
-		ll a, b, x, y; cin >> a >> b;
-		x = lcm(a,b);
-		y = gcd(a,b);
+		ll n; cin >> n;
+		bool bitAnd = n & (n - 1);
 		
-		cout << x << " " << y << "\n";
-		
+		// n on left is for case when n = 0
+		if(n && !bitAnd) {
+		    cout << "YES" << "\n";
+		} else {
+		    cout << "NO" << "\n";
+		}
     }
 	return 0;
 }
