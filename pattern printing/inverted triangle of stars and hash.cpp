de54c96c5ce7eb57a8b69dc06b347f6a7c3e6f4a
Rohan Kevin Broach <39668340+rkbroach@users.noreply.github.com>
2019-09-06 14:57:18 +0530

    Update inverted triangle of stars and hash.cpp

--- pattern printing/inverted triangle of stars and hash.cpp
+++ pattern printing/inverted triangle of stars and hash.cpp
@@ -9,8 +9,20 @@
 
 Problem statement - https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/friends-relationship-1/description/
 Time Complexity - O(n^2)
+Example - n = 9
+
+*################*
+**##############**
+***############***
+****##########****
+*****########*****
+******######******
+*******####*******
+********##********
+******************
 
 */
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -20,7 +32,7 @@ int main () {
     while (t--) {
         cin >> n;
         for (int i = 1; i <= n; i++) { // row
-            for (int j = 1; j <= 2*n; j++) {
+            for (int j = 1; j <= 2*n; j++) { // col
                 if (j <= i || j > 2*n-i) {
                     cout << "*";
                 } else {
