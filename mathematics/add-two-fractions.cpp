f402a2dd4b36a95741416c9eb82b199048b7ef01
Rohan Kevin Broach <39668340+rkbroach@users.noreply.github.com>
2019-09-06 14:53:41 +0530

    Update add-two-fractions.cpp

--- mathematics/add-two-fractions.cpp
+++ mathematics/add-two-fractions.cpp
@@ -7,33 +7,40 @@
 ██║  ██║██║  ██╗██████╔╝
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
 
+Time Complexity - O(1)
+Problem Statement - https://practice.geeksforgeeks.org/problems/add-two-fractions/1/?ref=self
+
 */
 
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-ll gcd(ll a, ll b) {
-    // modulo operator in Euclidean algorithm
-    if (b == 0) {
-        return a;
-    }
-    return gcd(b, a%b);
-}
+int gcd(int a, int b) { 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a%b); 
+} 
 
-ll lcm(ll a, ll b) {
-    return (  (a*b) / gcd(a,b)  );
+int lcm (int a, int b) {
+    return ( (a*b) / gcd(a,b) );
 }
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
 	int T; cin >> T;
 	while (T--) {
-		ll a, b, x, y; cin >> a >> b;
-		x = lcm(a,b);
-		y = gcd(a,b);
+		int num1, den1, num2, den2; cin >> num1 >> den1 >> num2 >> den2;
 		
-		cout << x << " " << y << "\n";
+		int denX = lcm(den1, den2); 
+		int numX = (num1)*(denX/den1) + (num2)*(denX/den2); // order of this formula matters
+	
+		// simplification  
+		int common_factor = gcd(numX,denX); 
+		denX = denX/common_factor; 
+		numX = numX/common_factor; 
+
+		// print
+		cout << numX << "/" << denX << "\n";
 		
     }
 	return 0;
