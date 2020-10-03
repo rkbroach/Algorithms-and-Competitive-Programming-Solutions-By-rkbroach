ffa2ea352dd2859e524bda1a1b25263d96ece4ce
Rohan Kevin Broach <39668340+rkbroach@users.noreply.github.com>
2020-10-03 19:08:33 +0530

    Update kadane-algorithm.cpp

--- kadane-algorithm.cpp
+++ kadane-algorithm.cpp
@@ -6,59 +6,32 @@
 |_|  |_|\_\_.__/|_|  \___/ \__,_|\___|_| |_|
 
 **/
+// https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0
+#include <bits/stdc++.h>
+using namespace std;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class GFG {
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
+int solve (int a[], int n) {
+    int global_max = a[0], local_max = a[0];
+    
+    for (int i = 1; i < n; i++) {
+        local_max  = max(a[i], local_max + a[i]);
+        global_max =  max(local_max, global_max);
     }
-
-    public static void main(String[] args) {
-        FastReader fr = new FastReader();
-        int t = fr.nextInt();
-        while (t-- > 0) {
-            int n = fr.nextInt();
-            int[] a = new int[n];
-            for (int i = 0; i < n; i++) {
-                a[i] = fr.nextInt();
-            }
-            int max_sum = find_max_sum(a, n);
-            System.out.println(max_sum);
-        }
-    }
-
-
-    static int find_max_sum(int[] a, int n) {
-        int global_max = a[0];
-        int local_max = a[0];
-
-        for (int i = 1; i < n; i++) {
-            local_max = Math.max(a[i], local_max + a[i]);
-            global_max = Math.max(local_max, global_max);
-        }
-        return global_max;
-    }
+    return global_max;
 }
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int t; cin >> t;
+	while(t--) {
+	    int n; cin >> n;
+	    int a[n];
+	    for (int i = 0; i < n; i++) {
+	        cin >> a[i];
+	    }
+	    int result = solve(a, n);
+	    cout << result << "\n";
+	}
+	return 0;
+}
+
