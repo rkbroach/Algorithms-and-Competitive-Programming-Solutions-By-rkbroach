3a1d7807ca24e0bd954428d73968197396568b96
Rohan Kevin Broach <39668340+rkbroach@users.noreply.github.com>
2020-10-06 01:30:15 +0530

    Create zig-zag.cpp

---
+++ zig-zag.cpp
@@ -0,0 +1,18 @@
+void zigZag(int a[], int n) {
+    bool flag = true;
+    for (int i = 0; i <= n - 2; i++) {
+        // < less than
+        if(flag) {
+            if (a[i] > a[i+1]) {
+                swap(a[i], a[i + 1]);
+            }
+        } else {
+            // > greater than
+            if (a[i] < a[i+1]) {
+                swap(a[i], a[i+1]);
+            }
+        }
+        // toggle the flag
+        flag = !flag;
+    }
+}
